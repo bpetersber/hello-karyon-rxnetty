4cc39858e3ffa060f9d5a24947521f454387e36d
Sebastiaan (Bas) Peters <sebastiaan.peters@orderbird.com>
2016-11-08 13:03:57 +0100

    ccc

--- src/main/java/com/kenzan/karyon/rxnetty/resource/IndexResource.java
+++ src/main/java/com/kenzan/karyon/rxnetty/resource/IndexResource.java
@@ -60,7 +60,7 @@ public class IndexResource implements RequestHandler<ByteBuf, ByteBuf>{
                             e.printStackTrace();
                         }
                         response.writeString("<html><head><style>body{text-align:center;font-family:'Lucida Grande'}</style></head><body><h2>Example Spinnaker Application</h2><h3>Instance Id " + instanceId + "</h3><h3>$USERDATA ENV VAR: " + userdata + "</h3></body></html>");
-			response.writeString("Hello, birds!");
+			response.writeString("Hello, orderbird!");
                         return response.close();
                     }
                 });
